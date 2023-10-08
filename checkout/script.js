62beea26a4ea7aecfe6c8a7d6c528c8683f1fbb1
bugra onal <bburak34569@gmail.com>
2023-10-09 02:11:45 +0300

    checkout ışlevsel

--- checkout/script.js
+++ checkout/script.js
@@ -1,32 +1,67 @@
 
-// Sepet işlevselliği -öylesine yazdım ihtiyaç olur
-let cartItems = [];
+document.addEventListener("DOMContentLoaded", function () {
+    // Function to calculate subtotal
+    function calculateSubtotal(quantity, price) {
+        return quantity * price;
+    }
 
-function addToCart(productName, price) {
-    cartItems.push({ productName, price });
-    updateCart();
-}
+    // Retrieve product data from local storage
+    let products = JSON.parse(localStorage.getItem('shopping_cart')) || [];
+    console.log(products);
 
-function updateCart() {
-    const cartContainer = document.querySelector('.cart');
-    cartContainer.innerHTML = '<h2>Sepetim</h2>';
+    // Reference to the table body, the total row, and the total price
+    const tableBody = document.getElementById('productTableBody');
+    const totalRow = document.createElement('tr'); // Create a new row for the total
+    let total = 0; // Initialize the total price
 
-    let total = 0;
+    // Function to create a row for a product
+    function createProductRow(product, index) {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${product.item.name}</td>
+            <td style="text-align: center">
 
-    cartItems.forEach(item => {
-        cartContainer.innerHTML += `
-            <div class="cart-item">
-                <img src="urun1.jpg" alt="${item.productName}">
-                <span>${item.productName} - $${item.price.toFixed(2)}</span>
-            </div>
+                <span>${product.quantity}</span>
+             
+            </td>
+            <td class="subtotal">$ ${calculateSubtotal(product.quantity, product.item.price)}</td>
         `;
 
-        total += item.price;
+
+        return row;
+    }
+
+    // Populate the table with product data and update the initial total
+    products.forEach((product, index) => {
+        const row = createProductRow(product, index);
+        tableBody.appendChild(row);
+
+        // Update the total price
+        total += calculateSubtotal(product.quantity, product.item.price);
     });
 
-    cartContainer.innerHTML += `<p>Toplam: $${total.toFixed(2)}</p>`;
-}
+    // Set the total row
+    totalRow.innerHTML = `
+    
+        <p class="Sum-of-checkout">Total: $${total}</p>
+    `;
+    
+    
+    tableBody.appendChild(totalRow);
+});
 
 
+// document.querySelector('checkout-button').addEventListener('click', function() {
+//     let card = document.getElementById('thank-you-card');
+//     card.style.display = 'block';
+//     card.style.opacity = '1';
+//     card.innerHTML = 'Thanks for shopping here!';
 
+//     setTimeout(function() {
+//         card.style.opacity = '0';
+//     }, 5000);
 
+//     setTimeout(function() {
+//         card.style.display = 'none';
+//     }, 5500);
+// });
